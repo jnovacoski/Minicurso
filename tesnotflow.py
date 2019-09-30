e4aa464121304cb218bfe9c8a7c5a0d2366a71e3
jnovacoski <sombrasdoinferno@gmail.com>
2019-09-30 09:17:02 -0300

    Adicionado um arquivo de python que podera ser usado para computacao natural

---
+++ tesnotflow.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+
+mnist = tf.keras.datasets.mnist
+
+def gen_image(arr):
+    two_d = (np.reshape(arr, (28, 28)) * 255).astype(np.uint8)
+    plt.imshow(two_d, interpolation='nearest')
+    return plt
+
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+im = gen_image(x_train[255])
+im.show()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
+
