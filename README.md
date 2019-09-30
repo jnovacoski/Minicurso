a6b7194bc723c5edf2f04c5785b3bcda5ea09f50
jnovacoski <sombrasdoinferno@gmail.com>
2019-09-30 16:30:32 -0300

    Adicionada referencia

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Computacao Natural - Redes neurais artificiais - Series Temporais
 
 O trabalho tem como objetivo a previsão de séries temporais, em específico, previsão de variação de preços nos índices da bolsa da bovespa, utilizando redes neurais artificiais, com linguagem Python, e as APIs tensorflow, keras, pandas, matplotlib, entre outros.
+
+
+https://www.tensorflow.org/tutorials/structured_data/time_series
--- novo.py
+++ novo.py
@@ -1,6 +1,32 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 
+df = pd.read_csv('indice.csv')
 
+del df['volume']
+
+df['data'] = pd.Categorical(df['data'])
+df['data'] = df.data.cat.codes
+
+ultimo = df.pop('ultimo')
+
+dataset = tf.data.Dataset.from_tensor_slices((df.values, ultimo.values))
+
+train_dataset = dataset.shuffle(len(df)).batch(1)
+
+def get_compiled_model():
+  model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(((5,), ()))),
+    tf.keras.layers.Dense(10, activation='relu'),
+    tf.keras.layers.Dense(10, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+  ])
+
+  model.compile(optimizer='adam',
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+  return model
+model = get_compiled_model(
+print(dataset)
